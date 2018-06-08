ee538269e5b7a55307d89348270116713405517d
Robert Dubrovskis <robert.dubrovskis@gmail.com>
2018-06-08 16:52:01 +0200

    Add project files.

---
+++ WebSite/Models/ImagePost.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace WebSite.Models
+{
+    public class ImagePost
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        //[NotMapped]
+        //public HttpPostedFileBase Image { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
---
+++ WebSite/Scripts/UserScripts.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    $('.modal').on('click', function () {
+        $('.modal.fade.in').css('display','flex');
+    });
+});
+
+$(document).ready(function () {
+    $(".menuimage").hover(
+        function () {
+            //$("img", this).fadeTo('fast', 0.5);
+            $(".captiontext", this).fadeTo('medium', 1)
+        },
+        function () {
+            //$("img", this).fadeTo('fast', 1);
+            $(".captiontext", this).fadeTo(100, 0);
+        }
+    );
+});
